631e09e44b3bd2755b8e39ac6e2c5b39e0d08328
dev <you@gmail.com>
2021-08-17 18:32:19 -0400

    validationFormCard-validationFormLogin-validationFormPin

---
+++ fastgi/Components/RadioButton.swift
@@ -0,0 +1,20 @@
+//
+//  RadioButton.swift
+//  fastgi
+//
+//  Created by Hegaro on 05/08/2021.
+//
+
+import SwiftUI
+
+struct RadioButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadioButton_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioButton()
+    }
+}
---
+++ fastgi/Components/RadioButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  RadioButtonView.swift
+//  fastgi
+//
+//  Created by Hegaro on 05/08/2021.
+//
+
+import SwiftUI
+
+struct RadioButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadioButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioButtonView()
+    }
+}
